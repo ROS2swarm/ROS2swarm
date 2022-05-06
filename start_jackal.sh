a12f6c3f355f3b74ff12d595be45eaa1ca0ff417
Tanja Kaiser <kaiser@iti.uni-luebeck.de>
2022-05-06 12:12:19 +0200

    version 1.1.0

--- start_jackal.sh
+++
@@ -1,3 +0,0 @@
-colcon build --symlink-install &&
- source ./install/setup.bash &&
-ros2 launch launch_turtlebot_gazebo jackal_environment.launch.py -w arena_large.world -p attraction_pattern -n 1 __log_level:=debug -r burger
