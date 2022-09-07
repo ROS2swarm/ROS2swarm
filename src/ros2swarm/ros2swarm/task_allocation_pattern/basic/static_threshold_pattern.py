136bd2b8c8c1623b79815ac33164cf27f59b2be2
Antoine <antoine.sion@unamur.be>
2022-09-07 15:17:12 +0200

    plugin added in task_allocation.world for global models states and clean up of the code in static_threshold_pattern.py

--- src/ros2swarm/ros2swarm/task_allocation_pattern/basic/static_threshold_pattern.py
+++ src/ros2swarm/ros2swarm/task_allocation_pattern/basic/static_threshold_pattern.py
@@ -15,6 +15,7 @@ import rclpy
 from ros2swarm.abstract_pattern import AbstractPattern
 from communication_interfaces.msg import IntListMessage
 from communication_interfaces.srv import ItemService
+from gazebo_msgs.msg import ModelStates
 from ros2swarm.utils import setup_node
 from ros2swarm.utils.state import State
 import random
@@ -44,12 +45,14 @@ class StaticThresholdPattern(AbstractPattern):
 
         self.robot_state = State.TASK_ALLOCATION
         self.items_list = []
+        self.model_states = ModelStates()
 
         self.has_item = False
         self.item_type_hold = None
         self.item_type_to_take = 0
 
         self.subscription = self.create_subscription(IntListMessage, '/items_list', self.item_list_callback, 10)
+        self.subscription = self.create_subscription(ModelStates, '/model_states/model_states', self.model_states_callback, 10)
         self.cli_item_service = self.create_client(ItemService, '/item_service')
 
         while not self.cli_item_service.wait_for_service(timeout_sec=1.0):
@@ -60,7 +63,7 @@ class StaticThresholdPattern(AbstractPattern):
         # self.timer_send_request = self.create_timer(10, self.send_request) #first we need to send a request to items_master
 
         # self.timer_check_future = self.create_timer(1, self.check_future) #then we check periodically if the request has been processed
-        sleep(random.uniform(1, 4))
+        # sleep(random.uniform(1, 4))
         self.timer = self.create_timer(1, self.swarm_command_controlled_timer(self.static_threshold_pattern_callback))
 
         self.threshold = random.uniform(0.5, 0.8)
@@ -89,6 +92,10 @@ class StaticThresholdPattern(AbstractPattern):
         # TODO implement condition for updating the list of items ONLY if the robot is in proximity of items_master
         self.items_list = msg.data
 
+    def model_states_callback(self, msg):
+        # the robot has access to the global position of all objects with this
+        self.model_states = msg
+
     def responseT(self, stimIntensity):
         output = stimIntensity ** self.n / (stimIntensity ** self.n + self.threshold)
         self.get_logger().info('Publishing {}:"{}"'.format(output, self.get_namespace()))
@@ -100,61 +107,38 @@ class StaticThresholdPattern(AbstractPattern):
         if (self.robot_state == State.TASK_ALLOCATION):
             # fancy algorithm based on static threshold allocation to implement
             is_all_zero = not np.any(self.items_list)
-            self.get_logger().info('The items %s' % self.items_list)
+            # self.get_logger().info('The items %s' % self.items_list)
             if (is_all_zero):
                 self.get_logger().info('No item s')
             else:
                 # calculate the probabilities for the stimulation (the normalized demand of each task)
                 # this goes in to the function T
-                taskdemand1 = self.items_list[0] / (self.items_list[0] + self.items_list[1] + self.items_list[2])
-                taskdemand2 = self.items_list[1] / (self.items_list[0] + self.items_list[1] + self.items_list[2])
-                taskdemand3 = self.items_list[2] / (self.items_list[0] + self.items_list[1] + self.items_list[2])
-                prob0 = self.responseT(taskdemand1)
-                prob1 = self.responseT(taskdemand2)
-                prob2 = self.responseT(taskdemand3)
-                probabilities = [prob0, prob1, prob2]
-                choose = [0, 1, 2]
-                item_take = 0
+                task_demand = []
+                prob = []
+                choose = []
+                for i in range(len(self.items_list)):
+                    task_demand.append(self.items_list[i] / sum(self.items_list))
+                    prob.append(self.responseT(task_demand[i]))
+                    choose.append(i)
 
-                # prob1 = (taskdemand1 / (taskdemand1 + 0.5))
-                # prob2 = (taskdemand2 / (taskdemand2 + 0.5))
-                # prob3 = (taskdemand3 / (taskdemand3 + 0.5))
-                self.get_logger().info('The Prob1 %s' % prob0)
-                self.get_logger().info('The Prob2 %s' % prob1)
-                self.get_logger().info('The Prob3 %s' % prob2)
+                item_taken = 0
 
-                chosen1 = random.choice(choose)
-                choose.remove(chosen1)
-                self.get_logger().info('Chosen prob is  {}: and now list is "{}"'.format(chosen1, choose))
+                while(len(choose) > 0 and item_taken == 0):
+                    chosen = random.choice(choose)
+                    choose.remove(chosen)
+                    rand = random.random()
+                    self.get_logger().info('The Rand %s' % rand)
+                    if (rand < prob[chosen]):
+                        item_taken = 1
+                        self.item_type_to_take = self.items_list.index(self.items_list[chosen])
 
-                chosen2 = random.choice(choose)
-                choose.remove(chosen2)
-                self.get_logger().info('Chosen prob2 is  {}: and now list is "{}"'.format(chosen2, choose))
+                if (item_taken == 0):
+                    self.robot_state = State.TASK_ALLOCATION
+                    self.get_logger().info('I have decided to not take an item !')
+                else:
+                    self.robot_state = State.DO_TASK
+                    self.get_logger().info('I have decided to take an item !')
 
-                chosen3 = random.choice(choose)
-                choose.remove(chosen3)
-                self.get_logger().info('Chosen prob3 is  {}: and now list is "{}"'.format(chosen3, choose))
-
-                rand = random.random()
-                self.get_logger().info('The Rand 1 %s' % rand)
-                if (rand < probabilities[chosen1]):
-                    item_taken = 1
-                    self.item_type_to_take = self.items_list.index(self.items_list[chosen1])
-                rand = random.random()
-                self.get_logger().info('The Rand 2 %s' % rand)
-                if (rand < probabilities[chosen2]):
-                    item_taken = 1
-                    self.item_type_to_take = self.items_list.index(self.items_list[chosen2])
-                rand = random.random()
-                self.get_logger().info('The Rand 3 %s' % rand)
-                if (rand < probabilities[chosen3]):
-                    item_taken = 1
-                    self.item_type_to_take = self.items_list.index(self.items_list[chosen3])
-                if (item_take == 0):
-                    self.item_type_to_take = self.items_list.index(max(self.items_list))  # just the max for now
-
-            self.get_logger().info('The items to move %s' % self.items_list)
-            self.robot_state = State.DO_TASK
 
         elif (self.robot_state == State.DO_TASK):
             # do the task here ! taking the item, moving, dropping the item, coming back
