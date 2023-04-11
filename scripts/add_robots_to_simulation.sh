192b057dc74888ec7c108b5d53e393c415501a74
Tanja Kaiser <tanjakkaiser@gmail.com>
2023-04-11 13:29:19 +0200

    bug fix sensor layer

--- scripts/add_robots_to_simulation.sh
+++ scripts/add_robots_to_simulation.sh
@@ -14,12 +14,14 @@
 #    limitations under the License.
 source ./install/setup.bash &&
  ros2 launch launch_gazebo add_robot.launch.py \
- start_index:=8 \
+ start_index:=4 \
  pattern:=random_walk_pattern \
  number_robots:=3 \
  log_level:=info \
- robot:=waffle_pi \
+ robot:=burger \
+ sensor_type:=lidar \
  version:=2
+ 
 # -p pattern_name
 ## movement pattern:
 #  * drive_pattern
